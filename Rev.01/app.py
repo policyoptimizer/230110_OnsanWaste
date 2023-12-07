d67c4a51f1f23da8041292d66bbf80cb59d8c7da
policyoptimizer <98368335+policyoptimizer@users.noreply.github.com>
2023-12-07 16:48:10 +0900

    ㅇ

--- Rev.01/app.py
+++ Rev.01/app.py
@@ -1,27 +1,42 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+import requests
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import numpy as np
 
-# 데이터와 모델 로드 (예시로 간단하게 처리)
+# 제목
+st.title('정제수 투입량 예측 애플리케이션')
+
+# 구글 시트 데이터 로드
+@st.cache
 def load_data():
-   # 데이터 로딩 로직
-   return features, target
+   file_id = "1h2n63h2EDSA6207eF15Gk2SjsN8qjI9TrWclCMQF6X0"
+   sheet_name = "Sheet1"
+   url = f"https://docs.google.com/spreadsheets/d/{file_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+   res = requests.get(url)
+   df = pd.read_csv(pd.compat.StringIO(res.text))
+   # 데이터 처리...
+   return df
 
-def train_model(features, target):
-   model = LinearRegression()
-   model.fit(features, target)
-   return model
+df = load_data()
 
-# Streamlit 앱
-st.title('모델 훈련 및 예측')
+# 데이터 표시 (옵션)
+if st.checkbox('데이터 표시'):
+   st.write(df.head())
 
-if st.button('모델 훈련'):
-   features, target = load_data()
-   model = train_model(features, target)
-   st.write('모델 훈련 완료')
-   
-# 예측 로직 (모델이 이미 훈련되어 있어야 함)
+# 예측을 위한 사용자 입력
+TOC = st.number_input('TOC 오염도', min_value=0)
+COD = st.number_input('COD 오염도', min_value=0)
+TN = st.number_input('T-N 오염도', min_value=0)
+Volume = st.number_input('오염물질 부피', min_value=0)
+
+# 예측 버튼
 if st.button('예측'):
-   # 예측 로직 구현
-   pass
+   # 모델 훈련 및 예측 로직
+   # ...
+
+# Streamlit 앱 실행: 
+
+streamlit run app.py
