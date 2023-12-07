7a2c430fc78706cb582039f893f16f293938fdfd
policyoptimizer <98368335+policyoptimizer@users.noreply.github.com>
2023-12-07 16:39:42 +0900

    d

--- Rev.01/app.py
+++ Rev.01/app.py
@@ -2,25 +2,25 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
-​
+
 # 데이터와 모델 로드 (예시로 간단하게 처리)
 def load_data():
    # 데이터 로딩 로직
    return features, target
-​
+
 def train_model(features, target):
    model = LinearRegression()
    model.fit(features, target)
    return model
-​
+
 # Streamlit 앱
 st.title('모델 훈련 및 예측')
-​
+
 if st.button('모델 훈련'):
    features, target = load_data()
    model = train_model(features, target)
    st.write('모델 훈련 완료')
-​
+   
 # 예측 로직 (모델이 이미 훈련되어 있어야 함)
 if st.button('예측'):
    # 예측 로직 구현
